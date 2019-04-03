8bba8fa5facaf690e8523a1b5a8d8a12f9db7eab
Jake Lee <jakehlee1@gmail.com>
2019-04-03 17:23:46 -0400

    Working obstacle drawing

--- draw_obstacles.py
+++ draw_obstacles.py
@@ -1,5 +1,6 @@
 import sys, os
 import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
 
 
@@ -9,11 +10,46 @@ def usage():
 class Drawer():
 	def __init__(self, ax):
 		self.ax = ax
-		fig = ax.get_figure()
+		self.fig = ax.get_figure()
 		self.cid = fig.canvas.mpl_connect('button_press_event', self)
+
+		self.new_obs = True
+		self.obs_list = []
+
+		self.curr_obs = []
+		self.curr_line,  = self.ax.plot([], [])
 
 	def __call__(self, event):
 		print('click', event)
+		if event.inaxes!=self.ax: return
+
+		raw_coord = [event.xdata, event.ydata]
+		round_coord = self.round_coord(raw_coord)
+
+		if self.new_obs:
+			self.new_obs = False
+			self.curr_obs = [round_coord]
+			self.curr_line.set_data(np.array(self.curr_obs)[:,0], np.array(self.curr_obs)[:, 1])
+		else:
+			if self.curr_obs[0] == round_coord:
+				self.ax.add_patch(matplotlib.patches.Polygon(self.curr_obs, closed=True))
+				self.obs_list.append(self.curr_obs)
+				self.curr_line.set_data([], [])
+				self.new_obs = True
+			else:
+				self.curr_obs.append(round_coord)
+				self.curr_line.set_data(np.array(self.curr_obs)[:,0], np.array(self.curr_obs)[:, 1])
+
+		self.fig.canvas.draw()
+
+
+
+	def round_coord(self, coord):
+		nd = np.array(coord)
+		nd = np.round(nd / 25.0) * 25
+		nd = nd.astype(int)
+		return nd.tolist()
+
 
 if __name__ == "__main__":
 	if len(sys.argv) != 3:
